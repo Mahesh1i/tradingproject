a2975c277c3dc63496f163be7021df86f80dd194
Mahesh1i <85782103+Mahesh1i@users.noreply.github.com>
2023-02-15 06:55:38 -0800

    update models.py
    adding models class

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class CSVFile(models.Model):
     file = models.FileField(upload_to='csv/')
     timeframe_in_minutes = models.IntegerField(default=10)
+    
+# addin another model class
+class JsonFile(models.Model):
+    name = models.CharField(max_field=30)
+    file_size = models.IntegerField()
